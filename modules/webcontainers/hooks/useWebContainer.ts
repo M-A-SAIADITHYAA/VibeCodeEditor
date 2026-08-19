83c0ab8e8aab179c236590dd0543baa66c5f8fc9
saiadithyaa <saiadithyaa2306@gmail.com>
2026-08-19 23:16:45 +0530

    s

--- modules/webcontainers/hooks/useWebContainer.ts
+++ modules/webcontainers/hooks/useWebContainer.ts
@@ -11,7 +11,7 @@ interface UseWebContaierReturn {
   isLoading: boolean;
   error: string | null;
   instance: WebContainer | null;
-  writeFileSync: (path: string, content: string) => Promise<void>;
+  writeFileSync: (path: string, content: string) => Promise<void>;jkjkj
   destory: () => void;
 }
 
